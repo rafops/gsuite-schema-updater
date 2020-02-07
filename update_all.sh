81221ca15f0283faf861a4a0738f5ddea1dcff56
Rafael Moraes <rafops@users.noreply.github.com>
2020-02-07 10:40:23 -0500

    adds config validation

--- update_all.sh
+++ update_all.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-if [ -f "credentials.json" ] ; then
-  python3 update_all.py
-else
+if ! [ -f "config.json" ] ; then
+  echo "config.json not found. Please check README.md"
+  exit 1
+fi
+
+if ! [ -f "credentials.json" ] ; then
   cat <<EOS
 
 1. Open the following web page and click “Enable the Directory API”:
@@ -20,4 +23,7 @@ else
 6. Run ./update_all.sh
 
 EOS
+  exit 1
 fi
+
+python3 update_all.py
