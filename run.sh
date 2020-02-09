a7af9a39de8a4de426433e3b272deaded336d678
Rafael Moraes <rafops@users.noreply.github.com>
2020-02-09 01:12:09 -0500

    fix typo and rename script

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ if ! [ -f "credentials.json" ] ; then
 
 5. Create and configure `config.json' from `config.json.example`
 
-6. Run ./update_all.sh
+6. Run ./run.sh
 
 EOS
   exit 1
@@ -30,7 +30,7 @@ fi
 if [ -n "$(uname -a | grep "^Linux ubuntu")" ] ; then
   docker run -it --rm -v "$(pwd)/:/root/workdir/" gsuite-schema-updater /root/workdir/run.sh
 elif [ -n "$(which python3)" ] ; then
-  python3 update_all.py
+  python3 update_target.py
 else
   echo "python3 not found. Please check README.md"
   exit 1
